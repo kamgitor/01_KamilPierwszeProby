10fdacbb011bcf8cb86946841177c7c02e572a69
Kamil <kamil@altel.pl>
2016-12-12 15:06:46 +0100

    .

--- Kamil Browser/Form1.cs
+++ Kamil Browser/Form1.cs
@@ -18,7 +18,44 @@ namespace Kamil_Browser
 
         private void baton_Click(object sender, EventArgs e)
         {
-            
+/*
+            Pracownik P1 = new Pracownik("Kamil", 1000);
+            Pracownik.ilosc = 0;
+*/
+
+            MojaKlasa p1 = new MojaKlasa();
+            p1.Dana = 5;
+            MojaKlasa p2;
+            MojaKlasa p3 = new MojaKlasa();
+            p3 = p1;        // 
+            p1.Dana = 10;
         }
+
+
+
+        public class MojaKlasa
+        {
+            public int Dana { get; set; }
+        }
+
+
+/*
+        public class Pracownik
+        {
+            public static int ilosc;
+            public string nazwisko;
+            private double zarobki;
+            public Pracownik(string n, double z) // konstruktor
+            {
+                nazwisko = n;
+                zarobki = z;
+            }
+            public void PokazPracownika() // metoda
+            {
+                // Console.WriteLine("{0,-15} {1,10}", nazwisko, zarobki);
+            }
+        }
+*/
+
     }
 }
